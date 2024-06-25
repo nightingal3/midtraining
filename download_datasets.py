6596024eb45a37966a1117cb3d5a616e713e3d1a
Emmy Liu <nightingal3@meta.com>
2024-06-24 19:35:40 -0700

    updates to train scripts

--- download_datasets.py
+++ download_datasets.py
@@ -1,9 +1,15 @@
 # (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 import argparse
-import os
-from datasets import load_dataset
-from functools import partial
 import json
+import os
+from functools import partial
+
+import dotenv
+from datasets import load_dataset
+
+
+dotenv.load_dotenv(dotenv_path="./configs/.env")
+MANIFOLD_DIR = os.environ["MANIFOLD_DIR"]
 
 datasets = {
     # TODO: natural instructions already downloaded, unzip and upload to manifold manually
@@ -46,11 +52,15 @@ def format_row(sample: dict, dataset: str) -> dict:
 
 
 def main(args: argparse.Namespace) -> None:
-    dataset_tuples = [(dataset, type_) for type_, datasets in datasets.items() for dataset in datasets]
+    dataset_tuples = [
+        (dataset, type_) for type_, datasets in datasets.items() for dataset in datasets
+    ]
     to_load = (
         dataset_tuples
         if args.type == "all"
-        else [(dataset, type_) for dataset, type_ in dataset_tuples if type_ == args.type]
+        else [
+            (dataset, type_) for dataset, type_ in dataset_tuples if type_ == args.type
+        ]
     )
 
     final_cols = ["instruction", "input", "output"]
@@ -61,15 +71,24 @@ def main(args: argparse.Namespace) -> None:
         else:
             dataset_dict = load_dataset(dataset, trust_remote_code=True)
         for split in dataset_dict.keys():
-            data_path = f"/data/users/nightingal3/datasets/{dataset_type}/{dataset}/{split}.json"
-            os.makedirs(os.path.dirname(data_path), exist_ok=True) 
-            formatted_dataset = dataset_dict[split].map(partial(format_row, dataset=dataset))
-            filtered_dataset = [dict(x) for x in formatted_dataset.remove_columns([col for col in formatted_dataset.column_names if col not in final_cols])]
-            
+            data_path = f"{MANIFOLD_DIR}/all_in_one_pretrain/datasets/{dataset_type}/{dataset}/{split}.json"
+            os.makedirs(os.path.dirname(data_path), exist_ok=True)
+            formatted_dataset = dataset_dict[split].map(
+                partial(format_row, dataset=dataset)
+            )
+            filtered_dataset = [
+                dict(x)
+                for x in formatted_dataset.remove_columns(
+                    [
+                        col
+                        for col in formatted_dataset.column_names
+                        if col not in final_cols
+                    ]
+                )
+            ]
+
             with open(data_path, "w") as f:
                 json.dump(filtered_dataset, f)
-
-        # TODO: upload to manifold if not exists there
 
 
 if __name__ == "__main__":
@@ -77,7 +96,10 @@ if __name__ == "__main__":
         "Download the instruction tuning/SFT datasets for this project"
     )
     parser.add_argument(
-        "--type", type=str, choices=["all", "sft", "instruction", "pretraining"], default="all"
+        "--type",
+        type=str,
+        choices=["all", "sft", "instruction", "pretraining"],
+        default="all",
     )
 
     args = parser.parse_args()
--- pretrain_then_finetune.sh
+++ pretrain_then_finetune.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 model_name=${1:-pythia-410m}
-checkpoint_dir=${2:-/data/users/nightingal3/all_in_one_pretraining/models/EleutherAI/pythia-410m}
+checkpoint_dir=${2:-/data/users/nightingal3/all_in_one_pretraining/models/EleutherAI/pythia-410m/}
 step=${3:-140000}
 total_steps=${4:-180000}
 max_seq_len=${5:-2048}
-
 
 conda activate towerllm-env
 export CUDA_VISIBLE_DEVICES="7"
@@ -18,6 +17,7 @@ pretraining_data_dir="${FINEWEB_DIR}"
 instruction_data_json="${MANIFOLD_DIR}/all_in_one_pretraining/datasets/sft/openai/gsm8k/train.json"
 
 # 1) Download a tokenizer
+echo -e "\033[32m> Downloading tokenizer \033[0m"
 litgpt download EleutherAI/$model_name \
   --tokenizer_only True
 
@@ -36,19 +36,34 @@ fi
 pretrained_checkpoint_dir="${checkpoint_dir}/step${step}"
 if [ $steps_remaining -gt 0 ]; then
   # TODO - train details such as batch size should be passed from a config instead of manually passed in.
+  # legit runs should have 20B. Test runs with 1B-5B for sanity checking
   echo -e "\033[32m> > Pretraining for ${steps_remaining} more steps\033[0m"
-
-  litgpt pretrain_mixed $model_name \
+  litgpt pretrain $model_name \
     --initial_checkpoint_dir "${checkpoint_dir}/step${step}" \
     --tokenizer_dir "${checkpoint_dir}/step${step}" \
-    --data MixedDataset \
-    --data.pretraining_data_path $pretraining_data_dir \
-    --data.sft_data_path $instruction_data_json \
+    --data FineWebDataset \
+    --data.data_path "${FINEWEB_DIR}" \
     --train.max_tokens "1_000_000_000" \
     --train.global_batch_size 1024 \
     --train.max_seq_len $max_seq_len \
-    --train.min_lr 1e-7 \
-    --out_dir "out/${model_name}_mixtrained_from_${step}"
+    --out_dir "out/${model_name}_pretrained_from_${step}"
 
   pretrained_checkpoint_dir="out/${model_name}_pretrained_from_${step}"
 fi
+
+# 3) Instruction-tune the model
+echo "\033[32m> > SFT \033[0m"
+litgpt finetune full --checkpoint_dir $pretrained_checkpoint_dir \
+  --data "JSON" \
+  --data.json_path $instruction_data_json \
+  --data.val_split_fraction 0.1 \
+  --train.max_seq_len $max_seq_len \
+  --logger_name wandb \
+  --out_dir "out/${model_name}_instruction_posttune"
+
+# TODO - need to add some tasks to eval harness.
+echo "> Evaluating"
+litgpt evaluate "out/${model_name}_instruction_posttune/final" \
+    --batch_size 8 \
+    --out_dir "eval_results/${model_name}_instruction_posttune" \
+    --tasks "gsm8k"
