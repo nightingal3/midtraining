a2db0caf16e2b3c64c736f4ebd3a5a864fb4944c
Emmy Liu <emmy@cmu.edu>
2025-10-16 17:12:10 -0500

    delete deprecated stuff

--- deploy/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Syncing back changes to the repo (not attempting to sync changes to conda)"
-
-set -ex
-scp -r /tmp/workspace-upperdir/* nightingal3@devgpu020.cln5.facebook.com:/data/users/nightingal3/all_in_one_pretraining/deploy
